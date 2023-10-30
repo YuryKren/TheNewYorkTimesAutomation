01b92a127c2a6cbdc958a32242d6d974122a1a9f
Yury Kren <yuryhumanoid@outlook.com>
2023-10-30 04:41:18 +0300

    create repository

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TheNewYorkTimesAutomation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://www.nytimes.com/";
+
+            driver.Close();
+            Console.WriteLine();
+        }
+    }
+}
