85e6c6b627ef706d15087701ef4db902855be186
Yury Kren <yuryhumanoid@outlook.com>
2023-11-02 02:09:24 +0300

    added HomePage class

--- Program.cs
+++ Program.cs
@@ -5,36 +5,19 @@ namespace TheNewYorkTimesAutomation
 {
     internal class Program
     {
-        static void CheckPage(IWebDriver driver, string xPath)
-        {
-            IWebElement webElement = driver.FindElement(By.XPath($"//div[@data-testid]//a[@data-navid='{xPath}']"));
-            webElement.Click();
-
-            Thread.Sleep(1000);
-            string titlePage = driver.Title;
-            Console.WriteLine("Title page {0} contains - {1}", xPath, titlePage.Contains(xPath));
-
-            string pageUrl = driver.Url;
-            Console.WriteLine("Page {0} URL contains - {1}", xPath, pageUrl.Contains(xPath.ToLower()));
-
-            driver.Url = "https://www.nytimes.com/";
-        }
-
         static void Main(string[] args)
         {
             IWebDriver driver = new ChromeDriver();
 
-            driver.Url = "https://www.nytimes.com/";
-            driver.Manage().Window.Maximize();
+            TNYTHomePage homePage = new(driver);
 
-            IWebElement agreePolicy = driver.FindElement(By.XPath("//button[text()='Continue']"));
-            agreePolicy.Click();
+            homePage.ClickAgreementOnCondition();
 
-            CheckPage(driver, "U.S.");
-            CheckPage(driver, "World");
-            CheckPage(driver, "Arts");
+            bool testCaseLifestyleNews= homePage.CheckPage("Lifestyle");
+            bool testCaseWorldNews = homePage.CheckPage("World");
+            bool testCaseArtsNews = homePage.CheckPage("Arts");
 
-
+            Console.WriteLine($"Test results: {testCaseLifestyleNews}, {testCaseWorldNews}, {testCaseArtsNews}");
 
             driver.Close();
 
---
+++ TNYTHomePage.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TheNewYorkTimesAutomation
+{
+    internal class TNYTHomePage
+    {
+        IWebDriver _driver;
+        const string HOME_PAGE = "https://www.nytimes.com/";
+        const string BUTTON_TO_AGREE_WITH_CHANGES = "//button[text()='Continue']";
+        const string MENU_ITEM_TEMPLATE = "//div[@data-testid]//a[@data-navid='{0}']";
+
+        public TNYTHomePage(IWebDriver webDriver) 
+        {
+            _driver = webDriver;
+            _driver.Url = HOME_PAGE;
+            _driver.Manage().Window.Maximize();
+        }
+
+        public void ClickAgreementOnCondition() 
+        {
+            IWebElement agreePolicy = _driver.FindElement(By.XPath(BUTTON_TO_AGREE_WITH_CHANGES));
+            agreePolicy.Click();
+        }
+
+        public bool CheckPage(string xPath)
+        {
+            string result = string.Format(MENU_ITEM_TEMPLATE, xPath);
+            IWebElement webElement = _driver.FindElement(By.XPath(result));
+            webElement.Click();
+
+            Thread.Sleep(1000);
+            string titlePage = _driver.Title;
+            string pageUrl = _driver.Url;
+
+            _driver.Url = HOME_PAGE;
+
+            return titlePage.Contains(xPath) && pageUrl.Contains(xPath.ToLower());
+        }
+    }
+}
